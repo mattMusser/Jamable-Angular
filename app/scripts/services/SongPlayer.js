d8585f137b76464a51b48564c836b8ef6fe8e6c1
Matt Musser <matthew.musser@gmail.com>
2017-06-22 14:41:13 -0500

    checkpoint 7 work in progress song is undefined

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,18 +3,20 @@
          var SongPlayer = {};
 
          SongPlayer.play = function(song) {
-             var currentBuzzObject = new buzz.soung(song.audioUrl, {
+
+             var currentBuzzObject = new buzz.sound(song.audioUrl, {
                  formats: ['mp3'],
                  preload: true
              });
-
+             console.log(song.audioUrl);
              currentBuzzObject.play();
+
          };
-         
+
          return SongPlayer;
     }
 
     angular
         .module('blocjams')
-        .factory('SongPlayer', Songplayer);
+        .factory('SongPlayer', SongPlayer);
 })();
