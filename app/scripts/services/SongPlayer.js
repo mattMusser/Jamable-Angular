f7a4ab36a45f93e63b952d5d7d2aa198dd3e9653
Matt Musser <matthew.musser@gmail.com>
2017-06-26 14:33:08 -0500

    Revert "Assignment 7 completed"
    This reverts commit c655c9d687e1bad8bb675140a49780eb438da743.

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -29,21 +29,11 @@
              currentSong = song;
          }
 
-         /**
-         *@function playSong
-         *@desc Plays current Buzz object and sets playing property of the song object to true
-         *@param {object} song
-         */
-         
-         var playSong = function(song) {
-             currentBuzzObject.play();
-             song.Playing = true;
-         }
-
          SongPlayer.play = function(song) {
              if (currentSong !== song) {
                  setSong(song);
-                 playSong(song);
+                 currentBuzzObject.play();
+                 song.playing = true;
                } else if (currentSong === song) {
                    if (currentBuzzObject.isPaused()) {
                        currentBuzzObject.play();
